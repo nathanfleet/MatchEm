efdea034d08ad2586a8446296ffc362d8236a9bf
Nathan Fleet <nfleet@kent.edu>
2024-09-26 21:48:14 -0400

    Rectangles generate in pairs

--- MatchEmScene/GameSceneViewController.swift
+++ MatchEmScene/GameSceneViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var timer: Timer?
+    var firstClickedRectangle: UIButton?
+    var score: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +21,33 @@ class ViewController: UIViewController {
         sender.removeFromSuperview()
     }
     
-    @objc func drawRectangles () {
-        let pair = generateRectanglePair()
-        for rect in pair {
-            self.view.addSubview(rect)
+    @objc func drawRectangles() {
+        let pair = self.generateRectanglePair()
+        for rect in pair { self.view.addSubview(rect) }
+    }
+
+    @objc func rectangleClicked(_ sender: UIButton) {
+        if firstClickedRectangle == nil {
+            firstClickedRectangle = sender
+            // TODO: make this more appealing
+            sender.layer.borderWidth = 5
+            sender.layer.borderColor = UIColor.white.cgColor
+        } else if firstClickedRectangle?.tag == sender.tag && firstClickedRectangle != sender {
+            score += 1
+            firstClickedRectangle?.removeFromSuperview()
+            sender.removeFromSuperview()
+        } else {
+            sender.layer.borderWidth = 0
+            firstClickedRectangle?.layer.borderWidth = 0
+            firstClickedRectangle = nil
         }
+        
     }
     
     func generateRectanglePair() -> [UIButton] {
         var rectangles: [UIButton] = []
         
-        // could maybe use a global static variable to ensure no two future
-        // pairs randomly have the same tag? idk
+        // TODO: use a global static variable instead
         let tag = Int.random(in: 1...1000)
         
         let randomWidth = CGFloat.random(in: 50...150)
@@ -45,21 +62,34 @@ class ViewController: UIViewController {
         
         for _ in 0..<2{
             let rectangle = UIButton()
-            // x, y from RandomRect
-            let x = CGFloat.random(in: self.view.frame.minX...(self.view.frame.maxX - randomWidth))
-            let y = CGFloat.random(in: self.view.safeAreaInsets.top...(self.view.frame.maxY - randomHeight - self.view.safeAreaInsets.bottom))
-            rectangle.frame.size = randomSize
+            var overlap: Bool
+            var frame: CGRect
+            repeat {
+                let x = CGFloat.random(in: self.view.frame.minX...(self.view.frame.maxX - randomWidth))
+                let y = CGFloat.random(in: self.view.safeAreaInsets.top...(self.view.frame.maxY - randomHeight - self.view.safeAreaInsets.bottom))
+                frame = CGRect(origin: CGPoint(x: x, y: y), size: randomSize)
+                overlap = false
+                for subview in self.view.subviews {
+                    if frame.intersects(subview.frame) {
+                        overlap = true
+                        break
+                    }
+                }
+            } while overlap
+                        
+            rectangle.frame = frame
             rectangle.backgroundColor = randomColor
-            rectangle.frame.origin = CGPoint(x: x, y: y)
             rectangle.tag = tag
+            rectangle.addTarget(self, action: #selector(rectangleClicked(_:)), for: .touchUpInside)
             rectangles.append(rectangle)
         }
         return rectangles
     }
     
     func startGame() {
-        // need a running timer ... when timer reaches 12 seconds kill the game
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(drawRectangles), userInfo: nil, repeats: true)
+        // TODO: initialize running timer which quits the game at 12 seconds
+        
+        timer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(drawRectangles), userInfo: nil, repeats: true)
     }
 }
 
